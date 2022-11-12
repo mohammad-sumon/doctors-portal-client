e22e2fd0c3b9828352ca313529cb41e3e16956e4
mohammad-sumon <sumon3011@gmail.com>
2022-11-12 21:57:18 +0600

    module 72.1 done

--- src/Pages/Home/Contact/Contact.js
+++ src/Pages/Home/Contact/Contact.js
@@ -8,7 +8,7 @@ const Contact = () => {
       className="py-16"
       style={{
         background: `url(${appointment})`,
-        backgroundSize: 'cover'
+        backgroundSize: "cover",
       }}
     >
       <div className="text-center">
@@ -18,25 +18,24 @@ const Contact = () => {
         </h1>
       </div>
       <div className="grid grid-flow-row text-center mt-10 gap-4">
-
         <input
           type="text"
           placeholder="Email Address"
-          className="input input-bordered input-sm w-1/4 mx-auto"
+          className="input input-bordered input-sm  w-96 lg:w-1/4 mx-auto"
         />
         <input
           type="text"
           placeholder="Subject"
-          className="input input-bordered input-sm w-1/4 mx-auto"
+          className="input input-bordered input-sm w-96 lg:w-1/4 mx-auto"
         />
         <input
           type="text"
           placeholder="Your Message"
-          className="input input-bordered input-lg w-1/4 mx-auto"
+          className="input input-bordered input-lg w-96 lg:w-1/4 mx-auto"
         />
       </div>
       <div className="text-center mt-10">
-      <PrimaryButton>Submit</PrimaryButton>
+        <PrimaryButton>Submit</PrimaryButton>
       </div>
     </section>
   );
