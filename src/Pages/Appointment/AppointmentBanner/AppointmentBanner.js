f7f6bb3c23b6fe06706a7f7a97079ed484622612
mohammad-sumon <sumon3011@gmail.com>
2022-11-12 22:38:59 +0600

    lift up state module 72.3 done

--- src/Pages/Appointment/AppointmentBanner/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner/AppointmentBanner.js
@@ -1,18 +1,22 @@
-import React, { useState } from "react";
 import chair from "../../../assets/images/chair.png";
 import { DayPicker } from "react-day-picker";
-import { format } from "date-fns";
+import bg from "../../../assets/images/bg.png";
 
-const AppointmentBanner = () => {
-  const [selectedDate, setSelectedDate] = useState(new Date());
+const AppointmentBanner = ({ selectedDate, setSelectedDate }) => {
   return (
-    <header className="my-6">
+    <header
+      className="my-6"
+      style={{
+        background: `url(${bg})`,
+        backgroundSize: "cover",
+      }}
+    >
       <div className="hero">
         <div className="hero-content flex-col lg:flex-row-reverse">
           <img
             src={chair}
             alt="dentist chair"
-            className="max-w-sm rounded-lg shadow-2xl"
+            className="max-w-sm lg:w-1/2 rounded-lg shadow-2xl"
           />
           <div className="mr-6">
             <DayPicker
@@ -20,7 +24,6 @@ const AppointmentBanner = () => {
               selected={selectedDate}
               onSelect={setSelectedDate}
             />
-            <p>You have selected date: {format(selectedDate, "PP")}</p>
           </div>
         </div>
       </div>
