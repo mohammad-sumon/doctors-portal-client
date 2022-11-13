8eeaf97879cc2b39a3e67bf52f98fcdf4421af91
Mohammad Sumon <sumon3011@gmail.com>
2022-11-13 12:04:57 +0600

    module 72.5 done

--- src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
@@ -1,9 +1,11 @@
 import { format } from "date-fns";
 import React, { useEffect, useState } from "react";
+import BookingModal from "../BookingModal/BookingModal";
 import AppointmentOption from "./AppointmentOption";
 
 const AvailableAppointments = ({ selectedDate }) => {
   const [appointmentOptions, setAppointmentOptions] = useState([]);
+  const [treatment, setTreatment] = useState(null);
 
   useEffect(() => {
     fetch("appointmentOptions.json")
@@ -21,9 +23,16 @@ const AvailableAppointments = ({ selectedDate }) => {
           <AppointmentOption
             key={option._id}
             appointmentOption={option}
+            setTreatment={setTreatment}
           ></AppointmentOption>
         ))}
       </div>
+      {
+        treatment &&
+        <BookingModal
+        treatment={treatment}
+      ></BookingModal>
+      }
     </section>
   );
 };
