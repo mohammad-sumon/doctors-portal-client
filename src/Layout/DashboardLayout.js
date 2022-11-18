ec08fe893ef2785457f1ca78eea7093a41a71f13
mohammad-sumon <sumon3011@gmail.com>
2022-11-18 21:53:44 +0600

    module 76.3 done

--- src/Layout/DashboardLayout.js
+++ src/Layout/DashboardLayout.js
@@ -29,6 +29,7 @@ const DashboardLayout = () => {
               <>
                 <li>
                   <Link to="/dashboard/allusers">All users</Link>
+                  <Link to="/dashboard/adddoctor">Add A Doctor</Link>
                 </li>
               </>
             )}
