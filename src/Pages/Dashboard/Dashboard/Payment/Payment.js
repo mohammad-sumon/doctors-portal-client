5120a4f9fd7a16162e3e39e2947354e47290b2ad
mohammad-sumon <sumon3011@gmail.com>
2022-11-19 23:39:15 +0600

    module 77.5 done

--- src/Pages/Dashboard/Dashboard/Payment/Payment.js
+++ src/Pages/Dashboard/Dashboard/Payment/Payment.js
@@ -1,12 +1,27 @@
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 import React from "react";
 import { useLoaderData } from "react-router-dom";
+import CheckoutForm from "./CheckoutForm";
+
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK);
+console.log(stripePromise);
 
 const Payment = () => {
-  const { data } = useLoaderData();
-
+  const booking = useLoaderData();
+  const { treatment, price, appointmentDate, slot } = booking;
   return (
     <div>
-      <h3 className="text-3xl">Payment</h3>
+      <h3 className="text-3xl">Payment for {treatment}</h3>
+      <p className="text-xl">
+        Please pay <strong>${price}</strong> for your appointment on{" "}
+        {appointmentDate} at {slot}
+      </p>
+      <div className="w-96 my-12">
+        <Elements stripe={stripePromise}>
+          <CheckoutForm />
+        </Elements>
+      </div>
     </div>
   );
 };
---
+++ src/Pages/Shared/DisplayError/DisplayError.js
@@ -0,0 +1,26 @@
+import React, { useContext } from "react";
+import { useRouteError } from "react-router-dom";
+import { AuthContext } from "../../../contexts/AuthProvider";
+
+const DisplayError = () => {
+  const { logOut } = useContext(AuthContext);
+  const error = useRouteError();
+
+  const handleLogOut = () => {
+    logOut()
+      .then(() => {})
+      .catch((error) => console.log(error));
+  };
+
+  return (
+    <div>
+      <p className="text-red-500">Something went wrong!!!</p>
+      <p className="text-red-400">{error.statusText || error.message}</p>
+      <h3 className="text-3xl">
+        Please <button onClick={handleLogOut}>Sign Out</button>and log back in
+      </h3>
+    </div>
+  );
+};
+
+export default DisplayError;
